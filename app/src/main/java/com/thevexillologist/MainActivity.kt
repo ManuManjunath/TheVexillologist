5ee46779b8d6b33916daeb42dbff38b489f45b93
ManuManjunath <manu.manjunath@target.com>
2020-12-28 18:48:32 +0530

    Code and layout for main activity

--- app/src/main/java/com/thevexillologist/MainActivity.kt
+++ app/src/main/java/com/thevexillologist/MainActivity.kt
@@ -2,10 +2,23 @@ package com.thevexillologist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start.setOnClickListener {
+            // Take name input before proceeding
+            if (et_name.text.toString().isNullOrEmpty()) {
+              Toast.makeText(this, "Please enter your name before proceeding", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                // Start InputParameters activity
+            }
+
+        }
     }
 }
